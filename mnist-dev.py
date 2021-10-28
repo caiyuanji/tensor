4c16352fcea9d5b6cf11f6fd5031367a392b498d
Meteor <caiyuanji@163.com>
2021-10-28 18:19:58 +0800

    add mnist-dev.py

---
+++ mnist-dev.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+class MnistModel(tf.keras.Model):
+    def __init__(self):
+        super(MnistModel, self).__init__()
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(128, activation='relu')
+        self.dense2 = tf.keras.layers.Dense(10, activation='softmax')
+
+    def call(self, x):
+        x = self.flatten(x)
+        x = self.dense1(x)
+        y = self.dense2(x)
+        return y
+
+model = MnistModel()
+
+model.compile(optimizer='adam', loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), metrics=['sparse_categorical_accuracy'])
+model.fit(x_train, y_train, batch_size=32, epochs=5, validation_data=(x_test, y_test), validation_freq=1)
+model.summary()
