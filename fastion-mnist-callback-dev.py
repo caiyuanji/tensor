f4b055dff854278c34cfae19da540e749a39de68
Meteor <caiyuanji@163.com>
2021-10-29 09:20:42 +0800

    Create fastion-mnist-callback-dev.py

---
+++ fastion-mnist-callback-dev.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+
+np.set_printoptions(threshold=np.inf)
+
+fashion = tf.keras.datasets.fashion_mnist
+(x_train, y_train), (x_test, y_test) = fashion.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=[28, 28]),
+    tf.keras.layers.Dense(300, activation='relu'),
+    tf.keras.layers.Dense(100, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['sparse_categorical_accuracy'])
+
+logdir = './callbacks'
+if not os.path.exists(logdir):
+    os.mkdir(logdir)
+output_model_file = os.path.join(logdir, "fashion_mnist_model.chk")
+if os.path.exists(output_model_file + '.index'):
+    print('-------------load the model-----------------')
+    model.load_weights(output_model_file)
+
+callbacks = [tf.keras.callbacks.TensorBoard(logdir),
+              tf.keras.callbacks.ModelCheckpoint(filepath=output_model_file, save_best_only=True, save_weights_only=True),
+              tf.keras.callbacks.EarlyStopping(patience=5, min_delta=1e-3)]
+
+history = model.fit(x_train, y_train, epochs=10,
+                    validation_data=(x_test, y_test),
+                    callbacks=callbacks)
+
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8, 5))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 1)
+    plt.show()
+
+plot_learning_curves(history)
+
+
+file = open('./fashion_weights.txt', 'w')
+for v in model.trainable_variables:
+    file.write(str(v.name) + '\n')
+    file.write(str(v.shape) + '\n')
+    file.write(str(v.numpy()) + '\n')
+file.close()
+
+
+acc = history.history['sparse_categorical_accuracy']
+val_acc = history.history['val_sparse_categorical_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plt.subplot(1, 2, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.title('Training and Validation Accuracy')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+plt.show()
